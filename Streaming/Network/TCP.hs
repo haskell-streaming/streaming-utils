661c841b7cc8db14a3d6ffc71876c4eebd8748d0
michaelt <what_is_it_to_do_anything@yahoo.com>
2016-02-07 20:55:20 -0500

    more tcp rearrangement

--- Streaming/Network/TCP.hs
+++ Streaming/Network/TCP.hs
@@ -1,24 +1,31 @@
 {-# LANGUAGE RankNTypes #-}
 
--- | This hyper-minimal module closely follows Renzo Carbonara's 'pipes-network' package.
---  It is meant to be used together with
--- the "Network.Simple.TCP" module from the @network-simple@ package, which is
--- completely re-exported from this module.
+-- | This hyper-minimal module closely follows the corresponding module 
+--   in Renzo Carbonara' 'pipes-network' package. It is meant to be used together with
+--   the "Network.Simple.TCP" module from Carbonara\'s @network-simple@ package, which is
+--   completely re-exported from this module. 
+
+-- | The material gives us occasion to meditate once again on the fact
+--   that if the @bytestring@ package had exported a correct streaming module, 
+--   which /it could have done with a minute but systematic typographical 
+--   alteration of @Data.ByteString.Lazy@/
+--   , then the entire Haskell infrastructure, including libraries like 
+--   @network-simple@, would have exported combinators like these from the beginning, 
+--   as a matter of course, almost without additional thought.
+
 
 module Streaming.Network.TCP (
   -- * Receiving
-  -- $receiving
     fromSocket
 
   -- * Sending
-  -- $sending
   , toSocket
   
   -- * Simple demos
   -- $demos
   
-  -- * Some commentary
-  -- $commentary  
+  -- * Source
+  -- $source 
   
   -- * Re-exports
   -- $exports
@@ -43,7 +50,6 @@ import           Control.Monad (when)
 import           Control.Monad.IO.Class
 --------------------------------------------------------------------------------
 
--- $receiving
 
 {- | Receives bytes from a connected socket with a maximum chunk size.
      The bytestream ends if the remote peer closes its side of the connection
@@ -74,7 +80,6 @@ fromSocket sock nbytes = loop where
 
 --------------------------------------------------------------------------------
 
--- $sending
 
 {- | Connect a stream of bytes to the remote end. The implementation
      is again very simple:
@@ -100,9 +105,166 @@ toSocket sock = loop where
 {-# INLINABLE toSocket #-}
 
 {- $demos
-  These mechanically follow the pleasantly transparent 'hello world'-ish 
-  examples of TCP connections in http://www.yesodweb.com/blog/2014/03/network-conduit-async .
 
+Here are a collection of little @hello telnet world@ programs, following Michael Snoyberg's <http://www.yesodweb.com/blog/2014/03/network-conduit-async post> on using @network-conduit@ 
+together with @async@ I hope the reader will find that they are a bit more intelligible when
+we think naively of 'byte streams' as ordinary Haskell entities, rather than conduits trapped in
+a framework. (In fact they're pretty straightforward either way, of course.) The complete source is appended to this module below.
+
+-   `serverToUpper` -  a server on 4000 that sends back input sent e.g. with telnet upper-cased
+-   `serverDouble`  -  a server on 4001 that sends back input doubled, `Char8` by `Char8`
+-   `clientToUpper`  -  a client through which the user interacts directly with the upper-caser
+-   `clientPipeline` -  a client that sends material to the  uppercasing server and then the doubling server and returns it to the user
+-   `proxyToUpper`  -  a proxy on 4002 that sends input to the uppercasing server on 4000
+-   `proxyAuth`  -  a proxy on 4003 that asks for demands  authorization before condescending to send user input to the upper-casing server on 4000
+
+The following remarks will require that eight
+instances of a terminal all be opened;   a
+crude option parser will make the examples usable with
+one executable:
+
+>    $ streaming-network-tcp-examples --help
+>    Usage: streaming-network-tcp-examples COMMAND
+
+>    Available options:
+>      -h,--help                Show this help text
+
+>    Available commands:
+>      ClientPipeline           
+>      ClientToUpper            
+>      ProxyAuth                
+>      ProxyToUpper             
+>      ServePipes               
+>      ServerDouble             
+>      ServerToUpper
+
+Since most examples use the uppercasing service,
+which looks like this:
+
+
+>    serverToUpper :: IO ()
+>    serverToUpper = do
+>        putStrLn "Opening upper-casing service on 4000"
+>        serve (Host "127.0.0.1") "4000" $ \(client,_) -> 
+>          fromSocket client 4096 -- raw bytes are received from a telnet user or the like
+>          & Q.map toUpper        -- we map them to uppercase
+>          & toSocket client      -- and send them back
+
+
+we start it in one terminal:
+
+>    term1$ streaming-network-tcp-examples ServerToUpper
+>    Opening upper-casing service on 4000
+    
+then in another terminal we can telnet to it:
+
+>    term2$ telnet localhost 4000
+>    Trying 127.0.0.1...
+>    Connected to localhost.
+>    Escape character is '^]'.
+>    hello -- <-- our input
+>    HELLO
+>    ...
+
+or we can scrap telnet and use a dedicated Haskell client. This is a little subtler:
+
+>    clientToUpper :: IO ()
+>    clientToUpper = connect "127.0.0.1" "4000" $ \(socket,_) -> do
+>      let act1 = toSocket socket Q.stdin           -- we send our stdin to the service
+>          act2 = Q.stdout (fromSocket socket 4096) -- we read our stdout from the service
+>      concurrently act1 act2                       -- but we do each on a separate thread
+>      return ()
+    
+Here, we stream standard input to the remote end indefinitely, and we stream 
+news from the remote end to standard output indefinitely. The two open ended processes are run them together
+with @Control.Concurrent.Async.concurrently@, so we see:
+
+>    term3$ streaming-network-tcp-examples ClientToUpper
+>    el pueblo unido jamas sera vencido!  -- our input
+>    EL PUEBLO UNIDO JAMAS SERA VENCIDO!
+>    el pueblo unido jamas sera vencido!  -- our input
+>    EL PUEBLO UNIDO JAMAS SERA VENCIDO!
+>    ...
+    
+To complicate the system of connections, we can also start a second server,
+which again just makes a trivial alteration in the bytestream, doubling each @Char8@:
+
+>    serverDoubler :: IO ()
+>    serverDoubler = do 
+>      putStrLn "Double server available on 4001"
+>      serve (Host "127.0.0.1") "4001" $ \(socket, remoteAddr) -> 
+>        fromSocket socket 4096                       -- raw bytes from a client
+>          & Q.toChunks                               -- are munged ...
+>          & S.map (B.concatMap (\x -> B.pack [x,x])) -- with standard bytestream materials
+>          & Q.fromChunks                             -- ...
+>          & toSocket socket                          -- and sent back
+
+
+starting it up thus:
+
+>     term4$ streaming-network-tcp-examples ServerDouble
+
+we see:
+
+>     term5$ telnet localhost 4001
+>     Trying 127.0.0.1...
+>     Connected to localhost.
+>     Escape character is '^]'.
+>     hello
+>     hheelllloo
+
+Now we complicate our use of the @async@ library with a 
+Haskell client that interacts with @4000@ and @4001@ together:
+
+>    clientPipeline :: IO ()
+>    clientPipeline = do
+>      putStrLn "We will connect stdin to 4000 and 4001 in succession."
+>      putStrLn "Input will thus be uppercased and doubled char-by-char.\n"
+>      connect "127.0.0.1" "4000" $ \(socket1,_) ->
+>        connect "127.0.0.1" "4001" $ \(socket2,_) ->
+>          do let act1 = toSocket socket1 Q.stdin
+>                                                     -- we send out stdin to the uppercaser
+>                 act2 = toSocket socket2 (fromSocket socket1 4096)
+>                                                     -- we send the results from the uppercase to the doubler
+>                 act3 = Q.stdout (fromSocket socket2 4096)
+>                                                     -- we send the doubler's output to stdout
+>             runConcurrently $ Concurrently act1 *>  -- all this simultaneously
+>                               Concurrently act2 *>
+>                               Concurrently act3
+
+Note the use of the `Applicative` instance for `Concurrently` from the
+`async` library to make the three stream operations simultaneous. Then we see:
+
+>    term6$ streaming-network-tcp-examples ClientPipeline
+>    hello
+>    HHEELLLLOO
+
+The upper-caser is open on @4000@ but don\'t tell the children. 
+The last program does a little manipulation of the bytestream to 
+demand authorization on 4003 
+
+>    term7$ streaming-network-tcp-examples ProxyAuth
+
+which then elsewhere permits
+
+>    term8$ telnet localhost 4003
+>    Trying 127.0.0.1...
+>    Connected to localhost.
+>    Escape character is '^]'.
+>    Username: spaceballs
+>    Password: 12345
+>    Successfully authenticated.
+>    hello
+>    HELLO
+>    hello!
+>    HELLO!
+    
+-}
+
+
+{- $source
+
+> -- streaming-network-tcp-examples.hs
 
 >{-#LANGUAGE OverloadedStrings #-}
 >module Main where
@@ -211,175 +373,5 @@ toSocket sock = loop where
 >      , command "ServerToUpper"  (info (pure serverToUpper) idm)
 >      ]
 >
-
--}
-
-{- $commentary
-
->The variants follow Michael S's text in this order:
->
->-   `serverToUpper`
->    -   a server on 4000 that sends back input sent e.g. with telnet
->        upper-cased or 'angry'
->-   `serverDouble`
->    -   a server on 4001 that sends back 
->        input doubled, `Char8` by `Char8`
->-   `clientToUpper`
->    -   a client through which the user interacts
->        directly to the "angry" server 
->-   `clientPipeline`
->    -   a client that sends material to the
->        "angry" server and the doubling server and
->        returns it to the user
->-   `proxyToUpper`
->    -   a proxy on 4002 that sends input to the
->        angry server on 4000
->-   `proxyAuth`
->    -   a proxy on 4003 that asks for demands
->        authorization before condescending to send
->        user input to the angry server on 4000
->
->The following remarks will require that eight
->instances of a terminal all be opened in the main
->directory of the repository; if you like you can
->`cabal install` from the main directory, and a
->crude option parser will make the examples usable with
->one executable:
->
->    $ streaming-network-tcp-examples --help
->    Usage: streaming-network-tcp-examples COMMAND
->
->    Available options:
->      -h,--help                Show this help text
->
->    Available commands:
->      ClientPipeline           
->      ClientToUpper            
->      ProxyAuth                
->      ProxyToUpper             
->      ServePipes               
->      ServerDouble             
->      ServerToUpper
->
->Since most examples use the uppercasing service,
->which looks like this:
->
->
->    serverToUpper :: IO ()
->    serverToUpper = do
->        putStrLn "Opening upper-casing service on 4000"
->        serve (Host "127.0.0.1") "4000" $ \(client,_) -> 
->          fromSocket client 4096 -- raw bytes are received from telnet user or the like
->          & Q.map toUpper        -- we map them to uppercase
->          & toSocket client      -- and send them back
->
->
->we start it in one terminal:
->
->    term1$ streaming-network-tcp-examples ServerToUpper
->    Opening upper-casing service on 4000
->    
->then in another terminal we can telnet to it:
->
->    term2$ telnet localhost 4000
->    Trying 127.0.0.1...
->    Connected to localhost.
->    Escape character is '^]'.
->    hello -- <-- our input
->    HELLO
->    ...
->
->or we can scrap telnet and use a dedicated Haskell client, which reads like this:
->
->    clientToUpper :: IO ()
->    clientToUpper = connect "127.0.0.1" "4000" $ \(socket,_) -> do
->      let act1 = toSocket socket Q.stdin           -- we send our stdin to the service
->          act2 = Q.stdout (fromSocket socket 4096) -- we read our stdout from the service
->      concurrently act1 act2 
->      return ()
->    
->thus: 
->
->    term3$ streaming-network-tcp-examples ClientToUpper
->    el pueblo unido jamas sera vencido!  -- our input
->    EL PUEBLO UNIDO JAMAS SERA VENCIDO!
->    el pueblo unido jamas sera vencido!  
->    EL PUEBLO UNIDO JAMAS SERA VENCIDO!
->    ...
->    
->To complicate things a bit, we can also start
->up the doubling service, which looks like this
->
->    serverDoubler :: IO ()
->    serverDoubler = do 
->      putStrLn "Double server available on 4001"
->      serve (Host "127.0.0.1") "4001" $ \(socket, remoteAddr) -> 
->        fromSocket socket 4096  -- raw bytes from a client
->          & Q.toChunks          -- are munged ...
->          & S.map (B.concatMap (\x -> B.pack [x,x]))
->          & Q.fromChunks
->          & toSocket socket     -- and sent back
->
->
->thus
->
->     term4$ streaming-network-tcp-examples ServerDouble
->
->then elsewhere
->
->     term5$ telnet localhost 4001
->     Trying 127.0.0.1...
->     Connected to localhost.
->     Escape character is '^]'.
->     hello
->     hheelllloo
->
->Now let's try the Haskell client that interacts with 4000 and 4001 together,
->i.e.:
->
->    clientPipeline :: IO ()
->    clientPipeline = do
->      putStrLn "We will connect stdin to 4000 and 4001 in succession."
->      putStrLn "Input will thus be uppercased and doubled char-by-char.\n"
->      connect "127.0.0.1" "4000" $ \(socket1,_) ->
->        connect "127.0.0.1" "4001" $ \(socket2,_) ->
->          do let act1 = toSocket socket1 Q.stdin
->                 -- we send out stdin to the uppercaser
->                 act2 = toSocket socket2 (fromSocket socket1 4096)
->                 -- we send the results from the uppercase to the doubler
->                 act3 = Q.stdout (fromSocket socket2 4096)
->                 -- we send the doubler's output to stdout
->             runConcurrently $ Concurrently act1 *>  -- all this simultaneously
->                               Concurrently act2 *>
->                               Concurrently act3
->
->(note the use of the `Applicative` instance for `Concurrently` from the
->`async` library), thus:
->
->    term6$ streaming-network-tcp-examples ClientPipeline
->    hello
->    HHEELLLLOO
->
->
->Don't tell the children they can access the
->uppercasing server directly on localhost 4000; we will
->demand authorization on 4003
->
->    term7$ streaming-network-tcp-examples ProxyAuth
->
->which then elsewhere permits
->
->    term8$ telnet localhost 4003
->    Trying 127.0.0.1...
->    Connected to localhost.
->    Escape character is '^]'.
->    Username: spaceballs
->    Password: 12345
->    Successfully authenticated.
->    hello
->    HELLO
->    hello!
->    HELLO!
->    
 
 -}
